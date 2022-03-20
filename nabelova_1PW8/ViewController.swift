98e6af4fa92f510618e06e0f1d9280094104b2d2
alkmnd <asfsdg>
2022-03-20 14:22:12 +0300

    Initial Commit

---
+++ nabelova_1PW8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nabelova_1PW8
+//
+//  Created by Наталья Белова on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
