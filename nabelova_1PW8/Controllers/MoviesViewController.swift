7d07d9d80d4ef411cc7d924741d2909bde30832f
alkmnd <asfsdg>
2022-03-20 15:23:42 +0300

    point 2

--- nabelova_1PW8/Controllers/MoviesViewController.swift
+++ nabelova_1PW8/Controllers/MoviesViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class MoviesViewController: UIViewController {
 
     private let tableView = UITableView()
+    private let apiKey = "fc57145e7f23876373951e70a3dd0560"
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         configureUI()
+        DispatchQueue.global(qos: .background).async {
+            [weak self] in self?.loadMovies()
+        }
     }
     private func configureUI() {
         view.addSubview(tableView)
@@ -28,6 +32,14 @@ class MoviesViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         tableView.reloadData()
+    }
+    
+    private func loadMovies() {
+        guard let url = URL(string: "https://api.themoviedb.org/3/discover/movie?api_key=(apiKey)&language=ruRu") else {
+            return assertionFailure("some problem with url")
+        }
+        let session = URLSession.shared.dataTask(with: URLRequest(url: url), completionHandler: { data, _, _ in})
+        session.resume()
     }
 }
 
